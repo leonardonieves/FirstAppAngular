7dfdb7d7ff4de3657f12e4bf798113b54a2b5ab8
leonardonieves <djleito.nieves@gmail.com>
2022-04-27 06:43:42 -0400

    Add Empleado using form

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleado } from '../empleado/empleado.model';
 
 @Component({
   selector: 'app-empleados',
@@ -7,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadosComponent implements OnInit {
 
+  empleados: Empleado[]= [
+    new Empleado("Juan", "Gonzalez","Director",1000),
+    new Empleado("Pepe", "Gonzalez","Director Adjunto",900),
+    new Empleado("Julio", "Gonzalez","Tecnico",800),
+    new Empleado("Lorenzo", "Gonzalez","Secretario",700),
+  ];
+
+  AddEmpleado(){
+    let empleado=new Empleado(this.campoNombre,this.campoApellido,this.campoCargo,this.campoSalario);
+    this.empleados.push(empleado);
+  }
+
+  campoNombre:string="";
+  campoApellido:string="";
+  campoCargo:string="";
+  campoSalario:number=0;
   constructor() { }
 
   ngOnInit(): void {
